452d4de09c780cbd438f3fdc5d2a5354f68ac7d8
gauz <me@gauz.net>
2022-10-18 19:53:36 +0200

    Test

--- resources/views/form/filepicker.blade.php
+++ resources/views/form/filepicker.blade.php
@@ -1,4 +1,4 @@
-<div class="{{$viewClass['form-group']}} {!! !$errors->has($errorKey) ? '' : 'has-error' !!}">
+<div id="morty_smith" class="{{$viewClass['form-group']}} {!! !$errors->has($errorKey) ? '' : 'has-error' !!}">
 
     <label for="{{$id}}" class="{{$viewClass['label']}} control-label">{{$label}}</label>
 
